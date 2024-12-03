0febd5c9f1d5a7f95b41f6cb9f44cea8d74ab652
Altair <166220493+PegasusA14@users.noreply.github.com>
2024-12-04 00:39:27 +0530

    feat:
    1.added the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import "./App.css";
-import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const url = window.location.href;
-  console.log(url);
   return (
     <>
-      {url === "http://localhost:5173/signup" && <SignupPage />}
-      {url === "http://localhost:5173/login" && <LoginPage />}
-      {url === "http://localhost:5173/" && <div>HomePage</div>}
+      <LoginPage />
     </>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import "../styles/LoginPage.css";
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="main-container">
+      <div className="left-container">
+        <img src="/left-banner.png" className="image-banner" />
+      </div>
+      <div className="right-container">
+        <img src="/logo.png" className="logo" />
+        <div className="header-wrapper">
+          <p className="title">Welcome Back!</p>
+          <p className="subtitle">Please enter your details</p>
+        </div>
+        <div className="input-wrapper">
+          <input type="email" placeholder="Email" className="input-element" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-element"
+          />
+          <div className="action-wrappers">
+            <div className="checkbox-wrapper">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="" className="checkbox-label">
+                Remember for 30 days
+              </label>
+            </div>
+            <p className="forgot-pws-txt">Forgot password ?</p>
+          </div>
+        </div>
+        <button className="login-btn">Log in</button>
+        <div className="signup-action-wrapper">
+          Didn&apos;t have an account? <a href="#">Sign up</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
